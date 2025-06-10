d30a65e5781af7f32745914320f3c7dc82aa50d4
peklos <vladoserov727@mail.ru>
2025-06-10 17:08:58 +0300

    dev deploy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
+// vue.config.js
+const { defineConfig } = require('@vue/cli-service');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  publicPath: './',
+  configureWebpack: {
+    plugins: [
+      // явно включаем плагин извлечения CSS
+      new MiniCssExtractPlugin({
+        // опции по желанию, например:
+        // filename: 'css/[name].[contenthash:8].css',
+        // chunkFilename: 'css/[name].[contenthash:8].css',
+      })
+    ]
+  }
+});
